00d5a56df4e2cacf2d75623020fc261953ff2db0
Britnell <tommy.britnell@gmail.com>
2026-02-14 15:15:36 +0100

    start chunking

--- node-tts/bun.ts
+++ node-tts/bun.ts
@@ -4,9 +4,10 @@ import { parse } from "node-html-parser"
 import { parseArgs } from "util"
 import { generateSpeech, type Voice } from "./kokoro.ts"
 
-const TARGET_MIN_TOKENS = 175
-const TARGET_MAX_TOKENS = 250
-const ABSOLUTE_MAX_TOKENS = 450
+const MIN_TOKENS = 100 // never below
+const OPTIMAL_TOKENS = 200 // optimal below
+const UPPER_TOKENS = 300 // ok below
+const MAX_TOKENS = 490 // never above
 
 // Simple token estimator (roughly 1 token per 4 characters for English)
 
@@ -46,11 +47,34 @@ async function readChapter(epub, i: number) {
 	await Bun.write(textFile, text)
 
 	//  make audio
-	console.log({ text })
+	chunkify(text)
 	// const audio = await generateSpeech({ text: chapterText, voice, speed })
 	// await audio.save(outputFile)
 }
 
+const nextPunct = (text: string, start: number) => {
+	const afterStart = text.slice(start)
+	const majorIndex = afterStart.search(/[\.\?\!]/)
+	const minorIndex = afterStart.search(/[,;:\-—]/)
+	return [
+		majorIndex >= 0 ? start + majorIndex : -1,
+		minorIndex >= 0 ? start + minorIndex : -1,
+	]
+}
+
+function chunkify(text: string) {
+	let pos = 0
+	console.log({ text })
+
+	for (let x = 0; x < 10; x++) {
+		const [mj, mn] = nextPunct(text, pos)
+		console.log({ mj, mn })
+		// if majoy is too long then use  minor
+		// if major is too short, get next major after that
+		// get that chunk of text
+	}
+	return
+}
 async function main() {
 	const epub = await EPub.createAsync(file, "./tmp", "./tmp")
 
